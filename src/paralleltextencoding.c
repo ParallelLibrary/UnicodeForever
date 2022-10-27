c41389a2f0da77e7c3b6667d302cb6b38d2065b2
MitchellWears <64507448+RetroGamingIsAwesome@users.noreply.github.com>
2022-10-26 21:13:50 -0400

    Add files via upload

--- src/paralleltextencoding.c
+++ src/paralleltextencoding.c
@@ -2,10 +2,17 @@
 
 size_t utf8_encoding(uint8_t src, uint8_t dst)
 {
-    /* BOM */
-    if (src == 0xEF && 0xBB && 0xBF)
-    {
-
-    }
-
+    int len;
+    uint8_t ch;
+    /* BOM UTF-8*/
+     if (len < 1)
+     {
+      if (src == 0xEF && src == 0xBB && src == 0xBF)
+      {
+      }
+      ch = src;
+     }
+      src += 3;
+      --len;
+      return 0;
 }
